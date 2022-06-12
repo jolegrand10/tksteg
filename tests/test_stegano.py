af7ad490548494656edfe63041fdafd81690f4f5
jolegrand10 <georges.georgoulis@wanadoo.fr>
2022-06-12 21:04:40 +0200

    Corrected file paths in unittests

--- tests/test_stegano.py
+++ tests/test_stegano.py
@@ -5,8 +5,8 @@ class TestStegano(unittest.TestCase):
 
     def test_read_encode_decode(self):
         s = Stegano()
-        s.read_image('image.jpg')
-        s.read_data('testinput.txt')
+        s.read_image('tests/image.jpg')
+        s.read_data('tests/testinput.txt')
         t = s.data
         s.encode()
         s.decode()
@@ -16,20 +16,20 @@ class TestStegano(unittest.TestCase):
 
     def test_read_encode_write_reread_decode(self):
         s = Stegano()
-        s.read_image('image.jpg')
-        s.read_data('testinput.txt')
+        s.read_image('tests/image.jpg')
+        s.read_data('tests/testinput.txt')
         t = s.data
         s.encode()
-        s.write_image('image2.png')
+        s.write_image('tests/image2.png')
         s.decode()
         t1 = s.data
         self.assertEqual(t, t1)
-        s.write_data('testoutput.txt')
-        s.read_image('image2.png')
+        s.write_data('tests/testoutput.txt')
+        s.read_image('tests/image2.png')
         s.decode()
         t1 = s.data #.decode('utf-8')
         self.assertEqual(t, t1)
-        s.read_data('testoutput.txt')
+        s.read_data('tests/testoutput.txt')
         t1 = s.data
         self.assertEqual(t, t1)
 
