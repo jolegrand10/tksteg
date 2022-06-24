06198a130adcbce20c2774e00c5825adbfad8c8b
Georges Georgoulis <georges.georgoulis@wanadoo.fr>
2022-06-24 19:25:08 +0200

    Built 1 file exe for Linux

--- setup.py
+++ setup.py
@@ -1,19 +1,32 @@
 from cx_Freeze import setup, Executable
+import sys
 
 # Dependencies are automatically detected, but it might need
 # fine tuning.
-build_options = {'packages': [], 'excludes': []}
 
-import sys
-base = 'Win32GUI' if sys.platform=='win32' else None
 
-executables = [
-    Executable('main.py', base=base, target_name = 'tksteg', icon="tksteg.ico", shortcutDir="DesktopFolder",
-               shortcutName="TkSteg",)
-]
+build_options = {'packages': [],
+                 'excludes': []}
 
-setup(name='tksteg',
-      version = '1.0',
-      description = 'Simple steganography',
-      options = {'build_exe': build_options},
-      executables = executables)
+
+if sys.platform=='win32':
+    base = 'Win32GUI'
+
+    executables = [
+        Executable('main.py', base=base, target_name = 'tksteg', icon="tksteg.ico", shortcutDir="DesktopFolder",
+                   shortcutName="TkSteg",)
+    ]
+
+    setup(name='tksteg',
+          version = '1.0',
+          description = 'Simple steganography',
+          options = {'build_exe': build_options},
+          executables = executables)
+elif sys.platform=='linux':
+    print("""To build a single file exe @ linux, use rather:
+   $ pyinstaller tksteg.spec
+or single line command:
+   $ pyinstaller main.py --hiddenimport PIL --hiddenimport PIL._imagingtk
+     --hiddenimport PIL._tkinter_finder --name tksteg""")
+else:
+    print("Only win and linux supported at the moment")
